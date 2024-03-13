e38fe048dd1778d792c722e42b4c5a6568a194db
animward <142351228+animward@users.noreply.github.com>
2024-03-13 14:50:38 -0700

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const reversed = word.split('').reverse().join('');
 
-module.exports = wordSearch
+  console.log(reversed);
+
+  const horizontalJoin = letters.map((ls) => ls.join(''));
+
+  // transpose matrix to check for vertical
+  const transpose = (matrix) =>
+    matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
+  const verticalJoin = transpose(letters).map((ls) => ls.join(''));
+
+  //console.log(horizontalJoin);
+
+  // check horizontal
+  for (l of horizontalJoin) {
+    // forwards
+    if (l.includes(word) || l.includes(reversed)) return true;
+    // backwards
+  }
+
+  // check vertically
+  for (const l of verticalJoin) {
+    // top down
+    if (l.includes(word) || l.includes(reversed)) return true;
+  }
+
+  return false;
+};
+
+/*const directions = [
+  [-1, 0][(1, 0)][(0, 1)][(0, -1)][(-1, 1)][(-1, 1)][(1, 1)][(1, -1)], // up // down // right // left // diagonal top right // diagonal top left // diagonal bottom right // diagonal bottom left
+];*/
+
+module.exports = wordSearch;
